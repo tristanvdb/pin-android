58ad811634a22bc61a99dc56dd8d395962d0f75d
Tristan Vanderbruggen <vanderbruggentristan@gmail.com>
2015-02-05 11:02:18 -0500

    Scan multiple versions

--- install.sh
+++ install.sh
@@ -1,42 +1,74 @@
 
-echo "Android SDK (r24.0.2) for Linux"
+### Path
+
+export PATH="`pwd`/android-sdk-linux/tools/":$PATH
+export PATH="`pwd`/android-sdk-linux/platform-tools/":$PATH
+
+### Download
+
+# Latest BusyBox for i686
+wget http://www.busybox.net/downloads/binaries/latest/busybox-i686
+
+# Android SDK (r24.0.2) for Linux
 wget http://dl.google.com/android/android-sdk_r24.0.2-linux.tgz
 tar xzf android-sdk_r24.0.2-linux.tgz
-echo "GUI config: Need to get Jelly Bean (4.2)"
-./android-sdk-linux/tools/android sdk
 
-alias android="`pwd`/android-sdk-linux/tools/android"
-echo "Create an Android Virtual Device"
-android create avd --name myavd --target android-17 --abi default/x86 --device 22
+# Pin 2.13 (r65163) for Android NDK9
+# Pin 2.14 (r67254) for Android NDK9
+# Pin 2.14 (r71313) for Android NDK9 & NDK10
+export PIN_VERSIONS= \
+  2.13-65163-gcc.4.6 \
+  2.14-67254-gcc.4.6 \
+  2.14-71313-gcc.4.6 \
+  2.14-71313-gcc.4.9
+for pin_version in $PIN_VERSION
+do
+  wget http://software.intel.com/sites/landingpage/pintool/downloads/pin-$pin_version.tar.gz
+  tar xzf pin-$pin_version.tar.gz
+done
 
-alias emulator="`pwd`/android-sdk-linux/tools/emulator64-x86"
-echo "Launch the virtual device"
-emulator -avd myavd &
+### Android SDK
 
-echo "Latest BusyBox for i686"
-wget http://www.busybox.net/downloads/binaries/latest/busybox-i686
-echo "Pin 2.14 for Android NDK r10"
-wget http://software.intel.com/sites/landingpage/pintool/downloads/pin-2.14-71313-gcc.4.9-android.tar.gz
-tar xzf pin-2.14-71313-gcc.4.9-android.tar.gz
+# Android 4.2.2, 4.3.1, 4.4.2, 4.4W.2, 5.0.1
+ANDROID_VERSION=17 18 19 20 21
 
-alias adb="`pwd`/android-sdk-linux/platform-tools/adb"
-echo "Installing Pin on the device. BusyBox is used for the 'tar' utility."
-adb shell mkdir /data/pin-android
-adb push busybox-i686 /data/pin-android
-adb push pin-2.14-71313-gcc.4.9-android/android-install.tar.gz /data/pin-android
-adb shell "cd /data/pin-android ; chmod 775 busybox-i686 ; ./busybox-i686 tar xzf android-install.tar.gz ; rm busybox-i686 android-install.tar.gz"
-adb shell "cd /data/pin-android ;  ./pin"
-adb shell "cd /data/pin-android ; LD_LIBRARY_PATH=/data/pin-android/ia32/runtime:/data/pin-android/ia32/stl/:\$LD_LIBRARY_PATH"
-adb shell "cd /data/pin-android ; BIN=/data/pin-android/ia32/bin:\$BIN LD_LIBRARY_PATH=/data/pin-android/ia32/runtime:/data/pin-android/ia32/stl/:\$LD_LIBRARY_PATH"
+# GUI config
+android sdk
 
-exit
+# Create Android Virtual Devices for each Android API
+for api in $ANDROID_VERSION
+do
+  android create avd --name device-$api --target android-$api --abi default/x86 --device 22
+done
 
-echo "Android NDK (r10) for Linux"
-wget http://dl.google.com/android/ndk/android-ndk-r10d-linux-x86_64.bin
-chmod +x android-ndk-r10d-linux-x86_64.bin
-./android-ndk-r10d-linux-x86_64.bin
-echo "Build the toolchain"
-./android-ndk-r10d/make-standalone-toolchain.sh --toolchain=x86-4.9 --platform=android-9 --install-dir=`pwd`/toolchain
 
-#rm android-sdk_r24.0.2-linux.tgz android-ndk-r10d-linux-x86_64.bin busybox-i686 pin-2.14-71313-gcc.4.9-android.tar.gz
+### PIN
+
+for api in $ANDROID_VERSION
+do
+  # Launch the virtual device
+  emulator -avd device-$api &
+
+  # Install busybox, used for its 'tar' utility.
+  adb push busybox-i686 /system/bin/
+  adb shell "chmod 775 /system/bin/busybox-i686"
+
+  # Install the different version of Pin
+  for pin_version in $PIN_VERSION
+  do
+    adb shell mkdir /data/pin-$pin_version
+    adb push pin-$pin_version-android/android-install.tar.gz /data/pin-$pin_version
+    adb shell "cd /data/pin-$pin_version ; busybox-i686 tar xzf android-install.tar.gz ; rm android-install.tar.gz"
+  done
+
+  # Test the different version of Pin
+  for pin_version in $PIN_VERSION
+  do
+    echo "Test Pin $pin_version on Android $api"
+    adb shell "/data/pin-$pin_version/pin -version"
+    adb shell "/data/pin-$pin_version/pin -- /system/bin/ls"
+  done
+
+  read -p "Close the emulator then press any key to continue..."
+done
 
